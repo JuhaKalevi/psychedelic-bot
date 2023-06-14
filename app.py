07da2fc62c2fdc55b115d96911b22478ddbfe997
Juha Kallio <bunnyh@psychedelic.fi>
2023-06-14 22:28:24 +0300

    Initial commit
    Bot is working mostly fine, streaming is not yet supported and we need to make it respond only when tagged outside threads.

---
+++ app.py
@@ -0,0 +1,41 @@
+from json import loads
+from os import environ
+import mattermostdriver
+import openai
+
+openai.api_key = environ['OPENAI_API_KEY']
+
+mm = mattermostdriver.Driver({
+  'url': environ['MATTERMOST_URL'],
+  'token': environ['MATTERMOST_TOKEN'],
+  'port': 443,
+})
+
+async def context_manager(event):
+  event = loads(event)
+  if 'event' in event and event['event'] == 'posted' and event['data']['sender_name'] != '@testgpt':
+    post = loads(event['data']['post'])
+    if post['root_id'] == "":
+      thread_id = post['id']
+      context = {'order': [post['id']], 'posts': {post['id']: post}}
+    else:
+      thread_id = post['root_id']
+      context = mm.posts.get_thread(post['id'])
+    context['order'].sort(key=lambda x: context['posts'][x]['create_at'])
+    messages = []
+    for post_id in context['order']:
+      if 'from_bot' in context['posts'][post_id]['props']:
+        role = 'assistant'
+      else:
+        role = 'user'
+      messages.append({'role': role, 'content': context['posts'][post_id]['message']})
+    openai_response = openai.ChatCompletion.create(model=environ['OPENAI_API_KEY'], messages=messages)
+    mm.posts.create_post(options={
+      'channel_id': post['channel_id'],
+      'message': openai_response['choices'][0]['message']['content'],
+      'file_ids': None,
+      'root_id': thread_id
+    })
+
+mm.login()
+mm.init_websocket(context_manager)
