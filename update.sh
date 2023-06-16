b73437879784a7407cc497ac982922e5beb7f5f0
Juha Kallio <bunnyh@psychedelic.fi>
2023-06-16 13:58:19 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,8 @@ case "$SSH_ORIGINAL_COMMAND" in
     git fetch --all
     git checkout $SSH_ORIGINAL_COMMAND
     git pull
-    docker compose pull
+    docker compose build
     docker compose up -d $SSH_ORIGINAL_COMMAND
-    docker pull gitlab.psychedelic.fi:5050/ai/bots/psychedelic-bot:latest
-    docker run -d gitlab.psychedelic.fi:5050/ai/bots/psychedelic-bot:latest
     ;;
   *)
     echo Permission denied.
