ca2deff1fbb7c09e6547b4c3b007501bba07381a
Juha Kallio <bunnyh@psychedelic.fi>
2023-06-18 14:10:53 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ case "$SSH_ORIGINAL_COMMAND" in
     git checkout $SSH_ORIGINAL_COMMAND
     git pull
     docker compose build
-    docker compose up -d $SSH_ORIGINAL_COMMAND
+    docker compose up -d $SSH_ORIGINAL_COMMAND --force-recreate
     ;;
   *)
     echo Permission denied.
