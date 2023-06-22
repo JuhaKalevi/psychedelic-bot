37d59025a1fc2572876e15a2b653e08c6b2bd0f8
Juha Kallio <bunnyh@psychedelic.fi>
2023-06-22 14:00:04 +0000

    Revert "Update app.py"
    This reverts commit 38761fd7cf81ffdbf09f524e72a3e440aa4dbb0f

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ webui_api = webuiapi.WebUIApi(host='kallio.psychedelic.fi', port=7860)
 webui_api.set_auth('useri', 'passu')
 
 def is_mainly_english(text):
-  encoding = chardet.detect(text.encode('utf-8'))["encoding"]
+  encoding = chardet.detect(text)["encoding"]
   decoded_text = text.decode(encoding)
   language = langdetect.detect(decoded_text)
   return language == "en"
 
 def generate_images(user_prompt, file_ids, post, count):
-  if not is_mainly_english(user_prompt.encode):
+  if not is_mainly_english(user_prompt.encode('utf-8')):
     return "Please use english only when generating images, for now."
   result = webui_api.txt2img(
     prompt = user_prompt,
