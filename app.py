9420133b1e9a171d5dc232e80f48aa952a6556b5
ronaz <ronaz@MAIN-PC>
2023-06-30 12:30:04 +0100

    Fix

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def is_mainly_english(text):
 def upscale_image(post, file_ids, resize_w: int = 1024, resize_h: int = 1024, upscaler="R-ESRGAN 4x+"):
   comment = ''
   if post['file_ids']:
-    image_file_info = mm.files.get_file_info(post['file_ids'][0])
+    image_file_info = mm.files.get_file(post['file_ids'][0])
     image_binary = mm.files.get_file(file_ids)
     image_path = os.path.join('/tmp', image_file_info['id'])
     try:
