7b5aa7ffeb611eb66dec24db6e13696437b1dceb
ronaz <ronaz@MAIN-PC>
2023-06-30 13:57:41 +0100

    Fix

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def upscale_image(file_ids, post, resize_w: int = 1024, resize_h: int = 1024, up
   comment = ''
   if post['file_ids']:
     image_file_info = mm.files.get_file(post['file_ids'][0])
-    image_binary = mm.files.get_file(file_ids)
+    image_binary = mm.files.get_file(file_ids[0])
     image_path = image_file_info['id'] + '.' + image_file_info['extension']
     try:
       with open(image_path, 'wb') as image_file:
