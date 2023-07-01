4dace0296c7e0c5c7412d2dac6af96b8af20183b
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 19:39:39 +0300

    async fix

--- api_mattermost.py
+++ api_mattermost.py
@@ -8,7 +8,7 @@ mm = mattermostdriver.Driver({
   'port':443
 })
 
-async def create_mattermost_post(channel_id, message, file_ids, thread_id):
+def create_mattermost_post(channel_id, message, file_ids, thread_id):
   try:
     mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
   except (ConnectionResetError, mattermostdriver.exceptions.InvalidOrMissingParameters, mattermostdriver.exceptions.ResourceNotFound) as err:
