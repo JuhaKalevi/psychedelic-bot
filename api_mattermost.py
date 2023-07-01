98a6f0bb7445230d426195b0b664e77a82c81eaf
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 18:02:14 +0300

    Revert api_mattermost.py to not being async

--- api_mattermost.py
+++ api_mattermost.py
@@ -4,9 +4,8 @@ from mattermostdriver.exceptions import InvalidOrMissingParameters, ResourceNotF
 
 mm = Driver({'url': environ['MATTERMOST_URL'], 'token': environ['MATTERMOST_TOKEN'], 'port': 443})
 
-async def create_mattermost_post(channel_id, message, file_ids, thread_id):
+def create_mattermost_post(channel_id, message, file_ids, thread_id):
   try:
-    response = await mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
-    print(response)
+    mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
   except (InvalidOrMissingParameters, ResourceNotFound) as err:
     print(f"Mattermost API Error: {err}")
