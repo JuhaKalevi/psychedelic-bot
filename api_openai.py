5dd6e1f186a13a5f7048a087faf3b9d352324431
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 17:45:42 +0300

    Fix api_openai.py

--- api_openai.py
+++ api_openai.py
@@ -5,7 +5,8 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 async def openai_chat_completion(messages, model=os.environ['OPENAI_MODEL_NAME']):
   try:
-    openai_response_content = await openai.ChatCompletion.create(model=model, messages=messages)['choices'][0]['message']['content']
+    response = await openai.ChatCompletion.create(model=model, messages=messages)
+    return response['choices'][0]['message']['content']
   except (openai.error.APIConnectionError,
           openai.error.APIError,
           openai.error.AuthenticationError,
@@ -14,5 +15,4 @@ async def openai_chat_completion(messages, model=os.environ['OPENAI_MODEL_NAME']
           openai.error.RateLimitError,
           openai.error.ServiceUnavailableError,
           openai.error.Timeout) as err:
-    openai_response_content = f"OpenAI API Error: {err}"
-  return openai_response_content
+    return f"OpenAI API Error: {err}"
