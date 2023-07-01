9dd70f2fdebe7e8ae7e34a543ec2291c536204e2
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 18:16:33 +0300

    Update api_mattermost.py

--- api_mattermost.py
+++ api_mattermost.py
@@ -1,6 +1,6 @@
 from os import environ
 from mattermostdriver import Driver
-from mattermostdriver.exceptions import InvalidOrMissingParameters, ResourceNotFound
+from mattermostdriver.exceptions import ConnectionResetError, InvalidOrMissingParameters, ResourceNotFound
 
 mm = Driver({'url': environ['MATTERMOST_URL'], 'token': environ['MATTERMOST_TOKEN'], 'port': 443})
 
@@ -8,5 +8,5 @@ async def create_mattermost_post(channel_id, message, file_ids, thread_id):
   try:
     response = await mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
     return response
-  except (InvalidOrMissingParameters, ResourceNotFound) as err:
+  except (ConnectionResetError, InvalidOrMissingParameters, ResourceNotFound) as err:
     return f"Mattermost API Error: {err}"
