ffeaf04f36b5ca43172364d6423a2c6ccc0c4b15
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 10:48:58 +0300

    Debugging

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ async def context_manager(event):
   if 'event' in event and event['event'] == 'posted' and event['data']['sender_name'] != os.environ['MATTERMOST_BOTNAME']:
     post = json.loads(event['data']['post'])
     if post['root_id'] == "":
-      if os.environ['MATTERMOST_BOTNAME'] not in post['message'] and not post['direct_message']:
+      print(post)
+      if os.environ['MATTERMOST_BOTNAME'] not in post['message']:
         return
       thread_id = post['id']
       if post['message'].lower().startswith("4x"):
