9a71c2f6f35efb838a42a6add4b40c996b3839ab
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 17:23:29 +0300

    Fix api_openai.py

--- api_openai.py
+++ api_openai.py
@@ -1,12 +1,18 @@
-from os import environ
-from openai import api_key, ChatCompletion
-from openai.error import APIConnectionError, APIError, AuthenticationError, InvalidRequestError, PermissionError, RateLimitError, ServiceUnavailableError, Timeout
+import os
+import openai
 
-api_key = environ['OPENAI_API_KEY']
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-async def openai_chat_completion(messages, model=environ['OPENAI_MODEL_NAME']):
+async def openai_chat_completion(messages, model=os.environ['OPENAI_MODEL_NAME']):
   try:
-    openai_response_content = await ChatCompletion.create(model=model, messages=messages)['choices'][0]['message']['content']
-  except (APIConnectionError, APIError, AuthenticationError, InvalidRequestError, PermissionError, RateLimitError, ServiceUnavailableError, Timeout) as err:
+    openai_response_content = await openai.ChatCompletion.create(model=model, messages=messages)['choices'][0]['message']['content']
+  except (openai.error.APIConnectionError,
+          openai.error.APIError,
+          openai.error.AuthenticationError,
+          openai.error.InvalidRequestError,
+          openai.error.PermissionError,
+          openai.error.RateLimitError,
+          openai.error.ServiceUnavailableError,
+          openai.error.Timeout) as err:
     openai_response_content = f"OpenAI API Error: {err}"
   return openai_response_content
