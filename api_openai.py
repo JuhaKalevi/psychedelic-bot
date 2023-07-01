17f67a3a0f4d76c08d735bbd9839435172bc3772
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 17:47:37 +0300

    Fix api_openai.py

--- api_openai.py
+++ api_openai.py
@@ -5,7 +5,7 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 async def openai_chat_completion(messages, model=os.environ['OPENAI_MODEL_NAME']):
   try:
-    response = await openai.ChatCompletion.create(model=model, messages=messages)
+    response = await openai.ChatCompletion.acreate(model=model, messages=messages)
     return response['choices'][0]['message']['content']
   except (openai.error.APIConnectionError,
           openai.error.APIError,
