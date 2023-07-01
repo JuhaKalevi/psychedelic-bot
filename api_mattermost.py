105c50706a7b1e1383a2674c9b8a813a9c1ec5db
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 17:59:02 +0300

    Async fix for Mattermost API

--- api_mattermost.py
+++ api_mattermost.py
@@ -6,6 +6,7 @@ mm = Driver({'url': environ['MATTERMOST_URL'], 'token': environ['MATTERMOST_TOKE
 
 async def create_mattermost_post(channel_id, message, file_ids, thread_id):
   try:
-    mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
+    response = await mm.posts.create_post(options={'channel_id':channel_id, 'message':message, 'file_ids':file_ids, 'root_id':thread_id})
+    print(response)
   except (InvalidOrMissingParameters, ResourceNotFound) as err:
     print(f"Mattermost API Error: {err}")
