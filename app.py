812aa4fcc5550eaedac39ff22817dab89a738479
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-01 19:03:07 +0300

    Async fix

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ async def context_manager(event):
   print(mm_response)
 
 async def run_chatbot():
-  login_status = await mm.login()
+  login_status = mm.login()
   print(login_status)
   await mm.init_websocket(context_manager)
 
