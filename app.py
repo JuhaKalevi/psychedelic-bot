5d92d8b10a603ecaa3b67ea6750d6e055fc61ad6
ronaz <mr.ronaz@gmail.com>
2023-07-06 22:49:37 +0300

    Testidebug youtube_transcription

--- app.py
+++ app.py
@@ -73,16 +73,7 @@ async def textgen_chat_completion(user_input, history):
 
 async def youtube_transcription(user_input):
   if user_input.startswith("transcribe @gpt3 "):
-    regex = r"\[(.*?)\]\((.*?)\)"
-    matches = re.findall(regex, user_input)
-    if matches:
-        # matches[0][1] will give the URL present in user_input
-      user_input = matches[0][1]
-    else:
-      return "Incorrect command format. Please use the following format: 'transcribe @gpt3 [http://youtubeURL](http://youtubeURL)'"
-    client = Client(TRANSCRIPTION_API_URI)
-    response = client.predict(user_input, fn_index=1)
-    print(response)
+    response = user_input
     return response
   return "Incorrect command format. Please use the following format: 'transcribe @gpt3 [http://youtubeURL](http://youtubeURL)'"
 
