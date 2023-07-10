4e40c9658f269a7d323233ece95a7390b36814f4
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-10 06:55:14 +0300

    Change directory to accomodate CI/CD jobs

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/juha/psychedelic-bot/venv"
+set -gx VIRTUAL_ENV "/builds/ai/bots/psychedelic-bot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
