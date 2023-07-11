fafd87030b8a2e08f64ab484d074d74f121d4d7d
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-11 20:10:38 +0300

    Async test

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,12 @@
 from os import environ
-from asyncio import get_event_loop
+from asyncio import run
 from mattermostdriver import Driver
 from context_manager import context_manager
 
-async def main():
+async def chatbot():
   mattermost_bot = Driver({'url':environ['MATTERMOST_URL'], 'token':environ['MATTERMOST_TOKEN'],'scheme':'https', 'port':443})
   await mattermost_bot.login()
   await mattermost_bot.init_websocket(context_manager)
 
-get_event_loop().run_until_complete(main())
+
+run(chatbot())
