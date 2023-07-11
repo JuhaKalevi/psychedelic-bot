16ab00b796425148ac1df4476f306091741ece7e
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-12 01:07:19 +0300

    Async fixes

--- basic_parsing.py
+++ basic_parsing.py
@@ -22,40 +22,44 @@ async def choose_system_message(post:dict, channel, analyze_code:bool=False) -> 
     default_system_message = [{'role':'system', 'content':'This is your code. Abstain from posting parts of your code unless discussing changes to them. Use 2 spaces for indentation and try to keep it minimalistic!'+'```'.join(code_snippets)}]
   return default_system_message
 
-async def generate_story_from_captions(captions:dict, model='gpt-4') -> str:
-  story = await openai_chat_completion([{'role':'user', 'content':(f"Make a consistent story based on these image captions: {captions}")}], model)
+async def generate_story_from_captions(message:str, model='gpt-4') -> str:
+  story = await openai_chat_completion([{'role':'user', 'content':(f"Make a consistent story based on these image captions: {message}")}], model)
   return story
 
 async def count_tokens(message:str) -> int:
   return len(tiktoken.get_encoding('cl100k_base').encode(json.dumps(message)))
 
-async def fix_image_generation_prompt(prompt:str) -> str:
-  fixed_prompt = await generate_text_from_message(f"convert this to english, in such a way that you are describing features of the picture that is requested in the message, starting from the most prominent features and you don't have to use full sentences, just a few keywords, separating these aspects by commas. Then after describing the features, add professional photography slang terms which might be related to such a picture done professionally: {prompt}")
+async def fix_image_generation_prompt(message:str) -> str:
+  fixed_prompt = await generate_text_from_message(f"convert this to english, in such a way that you are describing features of the picture that is requested in the message, starting from the most prominent features and you don't have to use full sentences, just a few keywords, separating these aspects by commas. Then after describing the features, add professional photography slang terms which might be related to such a picture done professionally: {message}")
   return fixed_prompt
 
-async def generate_text_from_message(message:dict, model='gpt-4') -> str:
+async def generate_text_from_message(message:str, model='gpt-4') -> str:
   response = await openai_chat_completion([{'role':'user', 'content':message}], model)
   return response
 
-async def is_asking_for_channel_summary(post:dict, channel, bot_name) -> bool:
-  if channel['purpose'] == f"{bot_name} always reply":
+async def is_asking_for_channel_summary(message:str, channel, bot_name:str) -> bool:
+  if channel['purpose'] == f"{bot_name} use channel context":
     return True
-  response = await generate_text_from_message(f'Is this a message where a summary of past interactions in this chat/discussion/channel is requested? Answer only True or False: {post["message"]}')
+  response = await generate_text_from_message(f'Is this a message where a summary of past interactions in this chat/discussion/channel is requested? Answer only True or False: {message}')
   return response.startswith('True')
 
-async def is_asking_for_code_analysis(post:dict, channel) -> bool:
-  if channel['display_name'] == 'GitLab' or post['message'].startswith('@code-analysis'):
-    return 'True'
-  response = await generate_text_from_message(f"Is this a message where knowledge or analysis of your code is requested? It does not matter whether you know about the files or not yet, you have a function that we will use later on if needed. Answer only True or False: {post['message']}")
+async def is_asking_for_code_analysis(message:str, channel) -> bool:
+  if channel['display_name'] == 'GitLab' or message.startswith('@code-analysis'):
+    return True
+  response = await generate_text_from_message(f"Is this a message where knowledge or analysis of your code is requested? It does not matter whether you know about the files or not yet, you have a function that we will use later on if needed. Answer only True or False: {message}")
   return response.startswith('True')
 
-async def is_asking_for_multiple_images(message:dict) -> bool:
+async def is_asking_for_image_generation(message:dict) -> bool:
+  response = await generate_text_from_message(f"Is this a message where an image is probably requested? Answer only True or False: {message}")
+  return response.startswith('True')
+
+async def is_asking_for_multiple_images(message:str) -> bool:
   response = await generate_text_from_message(f"Is this a message where multiple images are requested? Answer only True or False: {message}")
   return response.startswith('True')
 
-async def is_mainly_english(text:str) -> bool:
+def is_mainly_english(text:str) -> bool:
   response = langdetect.detect(text.decode(chardet.detect(text)["encoding"])) == "en"
   return response
 
-async def should_always_reply_on_channel(channel_purpose:str, bot_name:str) -> bool:
+def should_always_reply_on_channel(channel_purpose:str, bot_name:str) -> bool:
   return f"{bot_name} always reply" in channel_purpose
