cec24ad965f2489657708448e15c03ebea12d184
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-13 00:14:27 +0300

    Test

--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -9,32 +9,38 @@ import textgen_api
 async def context_manager(event):
   file_ids = []
   event = json.loads(event)
-  if 'event' in event and event['event'] == 'posted' and event['data']['sender_name'] != basic.bot_name:
-    post = json.loads(event['data']['post'])
-    magic_words_response = await respond_to_magic_words(post, file_ids)
-    if magic_words_response:
-      mattermost_api.create_post({'channel_id':post['channel_id'], 'message':magic_words_response, 'file_ids':file_ids, 'root_id':post['root_id']}, bot)
+  if not ('event' in event and event['event'] == 'posted' and event['data']['sender_name'] != basic.bot_name):
+    return
+  post = json.loads(event['data']['post'])
+  magic_words_response = await respond_to_magic_words(post, file_ids)
+  if magic_words_response:
+    mattermost_api.create_post({'channel_id':post['channel_id'], 'message':magic_words_response, 'file_ids':file_ids, 'root_id':post['root_id']}, bot)
+    return
+  message = post['message']
+  if post['root_id']:
+    reply_to = post['root_id']
+    summarize = await basic.is_asking_for_channel_summary(message)
+  else:
+    reply_to = post['id']
+  always_reply = basic.should_always_reply(mattermost_api.channel_from_post(post, bot)['purpose'])
+  if post['root_id'] == "" and (always_reply or basic.bot_name in message):
+    if summarize:
+      context = mattermost_api.channel_context(post, bot)
+    elif post['root_id']:
+      context = {'order':[post['id']], 'posts':{post['id']: post}}
     else:
       image_generation_response = await multimedia.consider_image_generation(bot, message, file_ids, post)
       if image_generation_response:
         mattermost_api.create_post({'channel_id':post['channel_id'], 'message':image_generation_response, 'file_ids':file_ids, 'root_id':reply_to}, bot)
+        context = mattermost_api.thread_context(post, bot)
         return
-      message = post['message']
-      if post['root_id']:
-        reply_to = post['root_id']
-      else:
-        reply_to = post['id']
-      always_reply = basic.should_always_reply(mattermost_api.channel_from_post(post, bot)['purpose'])
-      if always_reply or basic.bot_name in message:
-          summarize = await basic.is_asking_for_channel_summary(message)
-          if summarize:
-            context = mattermost_api.channel_context(post, bot)
-          elif post['root_id']:
-            context = mattermost_api.thread_context(post, bot)
-          else:
-            context = {'order':[post['id']], 'posts':{post['id']: post}}
-          response = await basic.generate_text_from_context(context)
-          mattermost_api.create_post({'channel_id':post['channel_id'], 'message':response, 'file_ids':file_ids, 'root_id':reply_to}, bot)
+    response = await basic.generate_text_from_context(context)
+    mattermost_api.create_post({'channel_id':post['channel_id'], 'message':response, 'file_ids':file_ids, 'root_id':reply_to}, bot)
+  else:
+    context = mattermost_api.thread_context(post, bot)
+    if any(basic.bot_name in post['message'] for post in context['posts'].values()):
+      response = await basic.generate_text_from_context(context)
+      mattermost_api.create_post({'channel_id':post['channel_id'], 'message':response, 'file_ids':file_ids, 'root_id':reply_to}, bot)
 
 async def respond_to_magic_words(post, file_ids):
   word = post['message'].lower()
