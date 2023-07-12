6d0e89ce051e4f2879026b00c7713694bc74a2b1
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-12 22:11:28 +0300

    Test

--- basic.py
+++ basic.py
@@ -7,8 +7,8 @@ import openai_api
 
 bot_name = os.environ['MATTERMOST_BOT_NAME']
 
-async def choose_system_message(post, channel):
-  if await is_asking_for_code_analysis(post['message'], channel):
+async def choose_system_message(post):
+  if await is_asking_for_code_analysis(post['message']):
     code_snippets = []
     for file_path in [x for x in os.listdir() if x.endswith('.py')]:
       with open(file_path, 'r', encoding='utf-8') as file:
@@ -45,10 +45,10 @@ async def fix_image_generation_prompt(message):
   fixed_prompt = await generate_text_from_message(f"convert this to english, in such a way that you are describing features of the picture that is requested in the message, starting from the most prominent features and you don't have to use full sentences, just a few keywords, separating these aspects by commas. Then after describing the features, add professional photography slang terms which might be related to such a picture done professionally: {message}")
   return fixed_prompt
 
-async def generate_text_from_context(context, channel, model='gpt-4'):
+async def generate_text_from_context(context, model='gpt-4'):
   if 'order' in context:
     context['order'].sort(key=lambda x: context['posts'][x]['create_at'], reverse=True)
-  system_message = await choose_system_message(context['posts'][context['order'][0]], channel)
+  system_message = await choose_system_message(context['posts'][context['order'][0]])
   context_messages = []
   context_tokens = await count_tokens(system_message)
   for post_id in context['order']:
@@ -56,7 +56,7 @@ async def generate_text_from_context(context, channel, model='gpt-4'):
       role = 'assistant'
     else:
       role = 'user'
-    message = {'role': role, 'content': context['posts'][post_id]['message']}
+    message = {'role':role, 'content':context['posts'][post_id]['message']}
     message_tokens = await count_tokens(message)
     print (f"generate_text_from_context: {context_tokens}")
     if context_tokens + message_tokens < 7777:
@@ -70,27 +70,18 @@ async def generate_text_from_context(context, channel, model='gpt-4'):
   return openai_response
 
 async def generate_text_from_message(message, model='gpt-4'):
-  print(f"generate_text_from_message: {message}")
   response = await openai_api.openai_chat_completion([{'role':'user', 'content':message}], model)
-  print(f"response {response}")
   return response
 
-async def is_asking_for_channel_summary(message, channel):
-  if channel['purpose'] == f"{bot_name} use channel context":
-    print(f"{channel['name']}: is_asking_for_channel_summary")
-    return True
+async def is_asking_for_channel_summary(message):
   response = await generate_text_from_message(f'Is this a message where a summary of past interactions in this chat/discussion/channel is requested? Answer only True or False: {message}')
   return response.startswith('True')
 
-async def is_asking_for_code_analysis(message, channel):
-  if"{bot_name} analyze your code" in channel['purpose']:
-    print(f"{channel['name']}: is_asking_for_code_analysis")
-    return True
+async def is_asking_for_code_analysis(message):
   response = await generate_text_from_message(f"Is this a message where knowledge or analysis of your code is requested? It does not matter whether you know about the files or not yet, you have a function that we will use later on if needed. Answer only True or False: {message}")
   return response.startswith('True')
 
 async def is_asking_for_image_generation(message):
-  print('is_asking_for_image_generation')
   response = await generate_text_from_message(f"Is this a message where an image is probably requested? Answer only True or False: {message}")
   return response.startswith('True')
 
@@ -100,10 +91,8 @@ async def is_asking_for_multiple_images(message):
 
 def is_mainly_english(text):
   response = langdetect.detect(text.decode(chardet.detect(text)["encoding"])) == "en"
-  print(f"is_mainly_english: {response}")
   return response
 
-def should_always_reply_on_channel(channel_purpose):
+def should_always_reply(channel_purpose):
   should_reply = f"{bot_name} always reply" in channel_purpose
-  print(f"should_always_reply_on_channel: {should_reply}")
   return should_reply
--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -9,36 +9,37 @@ import textgen_api
 async def context_manager(event):
   file_ids = []
   event = json.loads(event)
-  bot_message = None
+  return_signal = None
   if 'event' in event and event['event'] == 'posted' and event['data']['sender_name'] != basic.bot_name:
     post = json.loads(event['data']['post'])
-    bot_message = await respond_to_magic_words(post, file_ids)
-    if bot_message:
-      mattermost_api.create_post({'channel_id':post['channel_id'], 'message':bot_message, 'file_ids':file_ids, 'root_id':post['root_id']}, bot)
+    return_signal = await respond_to_magic_words(post, file_ids)
+    if return_signal:
+      mattermost_api.create_post({'channel_id':post['channel_id'], 'message':return_signal, 'file_ids':file_ids, 'root_id':post['root_id']}, bot)
     else:
       message = post['message']
       channel = mattermost_api.channel_from_post(post, bot)
-      always_reply = basic.should_always_reply_on_channel(channel['purpose'])
-      if always_reply or basic.bot_name in message:
+      always_reply = basic.should_always_reply(channel['purpose'])
+      if post['root_id']:
         reply_to = post['root_id']
-        bot_message = await multimedia.consider_image_generation(bot, message, file_ids, post)
-        if not bot_message:
-          summarize = await basic.is_asking_for_channel_summary(message, channel)
+      else:
+        reply_to = post['id']
+      if always_reply or basic.bot_name in message:
+        return_signal = await multimedia.consider_image_generation(bot, message, file_ids, post)
+        if not return_signal:
+          summarize = await basic.is_asking_for_channel_summary(message)
           if summarize:
             context = mattermost_api.channel_context(post, bot)
           else:
             context = mattermost_api.thread_context(post, bot)
-          bot_message = await basic.generate_text_from_context(context, channel)
-      elif basic.bot_name in message:
-        reply_to = post['root_id']
+          return_signal = await basic.generate_text_from_context(context, channel)
         context = await basic.generate_text_from_message(message)
       else:
         reply_to = post['root_id']
         context = mattermost_api.thread_context(post, bot)
         if any(basic.bot_name in context_post['message'] for context_post in context['posts'].values()):
-          bot_message = await basic.generate_text_from_context(context, channel)
-      if bot_message:
-        mattermost_api.create_post({'channel_id':post['channel_id'], 'message':bot_message, 'file_ids':file_ids, 'root_id':reply_to}, bot)
+          return_signal = await basic.generate_text_from_context(context, channel)
+      if return_signal:
+        mattermost_api.create_post({'channel_id':post['channel_id'], 'message':return_signal, 'file_ids':file_ids, 'root_id':reply_to}, bot)
 
 async def respond_to_magic_words(post, file_ids):
   word = post['message'].lower()
