8449a795fecb2b02a0adf6ff2aedfddfba8ea36a
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-12 17:25:36 +0300

    More detailed debug logging

--- mattermost_api.py
+++ mattermost_api.py
@@ -1,15 +1,18 @@
 import mattermostdriver.exceptions
 
+def _channel_display_name_from_post(post, bot):
+  return bot.channels.get_channel(post['channel_id'])['display_name']
+
 def channel_context(post, bot):
-  print('channel_context')
+  print(f"{_channel_display_name_from_post(post)}: len(channel_context): {len(channel_context)}")
   return bot.posts.get_posts_for_channel(post['channel_id'])
 
 def channel_from_post(post, bot):
-  print('channel_from_post')
+  print(f"{_channel_display_name_from_post(post)}: channel_from_post")
   return bot.channels.get_channel(post['channel_id'])
 
 def create_post(options, bot):
-  print('create_post')
+  print(f"create_post: {options}")
   try:
     bot.posts.create_post(options=options)
   except (ConnectionResetError, mattermostdriver.exceptions.InvalidOrMissingParameters, mattermostdriver.exceptions.ResourceNotFound) as err:
@@ -19,7 +22,7 @@ def get_mattermost_file(file_id, bot):
   return bot.files.get_file(file_id=file_id)
 
 def thread_context(post, bot):
-  print('thread_context')
+  print(f"{_channel_display_name_from_post(post)}: thread_context'")
   return bot.posts.get_thread(post['id'])
 
 def upload_mattermost_file(channel_id, files, bot):
