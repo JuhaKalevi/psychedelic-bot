2f3af20ea8a61dceb57047bca314c854083fd9f1
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-12 18:08:52 +0000

    Revert "Test fix"
    This reverts commit 0ff8956b1fc6a11104da46071ce09fdec5b1f500

--- mattermost_api.py
+++ mattermost_api.py
@@ -1,30 +1,30 @@
 import mattermostdriver.exceptions
 
-async def _channel_display_name_from_post(post, bot):
+def _channel_display_name_from_post(post, bot):
   return bot.channels.get_channel(post['channel_id'])['display_name']
 
-async def channel_context(post, bot):
+def channel_context(post, bot):
   context = bot.posts.get_posts_for_channel(post['channel_id'])
   print(f"{_channel_display_name_from_post(post, bot)}: len(channel_context): {len(context)}")
   return context
 
-async def channel_from_post(post, bot):
+def channel_from_post(post, bot):
   print(f"{_channel_display_name_from_post(post, bot)}: channel_from_post")
   return bot.channels.get_channel(post['channel_id'])
 
-async def create_post(options, bot):
+def create_post(options, bot):
   try:
     bot.posts.create_post(options=options)
   except (ConnectionResetError, mattermostdriver.exceptions.InvalidOrMissingParameters, mattermostdriver.exceptions.ResourceNotFound) as err:
     print(f'ERROR mattermost.posts.create_post(): {err}')
 
-async def get_mattermost_file(file_id, bot):
+def get_mattermost_file(file_id, bot):
   return bot.files.get_file(file_id=file_id)
 
-async def thread_context(post, bot):
+def thread_context(post, bot):
   print(f"{_channel_display_name_from_post(post, bot)}: thread_context")
   return bot.posts.get_thread(post['id'])
 
-async def upload_mattermost_file(channel_id, files, bot):
+def upload_mattermost_file(channel_id, files, bot):
   print('upload_mattermost_file')
   return bot.files.upload_file(channel_id, files=files)['file_infos'][0]['id']
