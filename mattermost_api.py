058ed1db1d55b4b45f2094c4a0f3edc7948aa42c
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-12 03:08:37 +0300

    More debug prints

--- mattermost_api.py
+++ mattermost_api.py
@@ -1,12 +1,15 @@
 import mattermostdriver.exceptions
 
 def channel_context(post, bot):
+  print('channel_context')
   return bot.posts.get_posts_for_channel(post['channel_id'])
 
 def channel_from_post(post, bot):
+  print('channel_from_post')
   return bot.channels.get_channel(post['channel_id'])
 
 def create_post(options, bot):
+  print('create_post')
   try:
     bot.posts.create_post(options=options)
   except (ConnectionResetError, mattermostdriver.exceptions.InvalidOrMissingParameters, mattermostdriver.exceptions.ResourceNotFound) as err:
@@ -16,7 +19,9 @@ def get_mattermost_file(file_id, bot):
   return bot.files.get_file(file_id=file_id)
 
 def thread_context(post, bot):
+  print('thread_context')
   return bot.posts.get_thread(post['id'])
 
 def upload_mattermost_file(channel_id, files, bot):
+  print('upload_mattermost_file')
   return bot.files.upload_file(channel_id, files=files)['file_infos'][0]['id']
