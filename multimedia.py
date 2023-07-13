9e9d95fa0c7f3210def4bf70942e5e1162c9941b
Mr ronaz <mr.ronaz@gmail.com>
2023-07-13 05:43:00 +0000

    Reverting pix2pix-function to what used to be a functional pix2pix function

--- multimedia.py
+++ multimedia.py
@@ -122,35 +122,45 @@ async def youtube_transcription(user_input):
     ytsummary = await basic.generate_summary_from_transcription(prediction)
     return ytsummary
 
-async def instruct_pix2pix(bot, file_ids, post):
+async def instruct_pix2pix(file_ids, post):
   comment = ''
-  for input_image_id in post['file_ids']:
-    file_response = mattermost_api.get_mattermost_file(input_image_id, bot)
+  for post_file_id in post['file_ids']:
+    file_response = mm.files.get_file(file_id=post_file_id)
     if file_response.status_code == 200:
-      file_type = path.splitext(file_response.headers["Content-Disposition"])[1][1:]
-      post_file_path = f'{input_image_id}.{file_type}'
-      async with open(post_file_path, 'wb') as new_image:
-        new_image.write(file_response.content)
+      file_type = os.path.splitext(file_response.headers["Content-Disposition"])[1][1:]
+      post_file_path = f'{post_file_id}.{file_type}'
+      with open(post_file_path, 'wb') as post_file:
+        post_file.write(file_response.content)
     try:
-      post_file_image = PIL.Image.open(post_file_path)
+      post_file_image = Image.open(post_file_path)
       options = webui_api.get_options()
       options = {}
       options['sd_model_checkpoint'] = 'instruct-pix2pix-00-22000.safetensors [fbc31a67aa]'
       options['sd_vae'] = "None"
       webui_api.set_options(options)
-      result = webui_api.img2img(images=[post_file_image], prompt=post['message'], steps=150, seed=-1, cfg_scale=7.5, denoising_strength=1.5)
+      result = webui_api.img2img(
+        images = [post_file_image],
+        prompt = post['message'],
+        steps = 150,
+        seed = -1,
+        cfg_scale = 7.5,
+        denoising_strength=1.5,
+      )
       if not result:
         raise RuntimeError("API returned an invalid response")
-      processed_image_path = f"processed_{input_image_id}.png"
+      processed_image_path = f"processed_{post_file_id}.png"
       result.image.save(processed_image_path)
-      async with open(processed_image_path, 'rb') as image_file:
-        file_id = mattermost_api.upload_mattermost_file(post['channel_id'], {'files': (processed_image_path, image_file)}, bot)
+      with open(processed_image_path, 'rb') as image_file:
+        file_id = mm.files.upload_file(
+          post['channel_id'],
+          files={'files': (processed_image_path, image_file)}
+        )['file_infos'][0]['id']
       file_ids.append(file_id)
       comment += "Image processed successfully"
     except RuntimeError as err:
       comment += f"Error occurred while processing image: {str(err)}"
     finally:
       for temporary_file_path in (post_file_path, processed_image_path):
-        if path.exists(temporary_file_path):
-          remove(temporary_file_path)
+        if os.path.exists(temporary_file_path):
+          os.remove(temporary_file_path)
   return comment
