9f234163631a7cd739455e1cdcad19e94eec51b5
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-16 22:20:45 +0300

    Install pip requirements when deploying

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ git fetch --all
 git checkout $SSH_ORIGINAL_COMMAND
 git pull
 . venv/bin/activate
+pip install -r requirements.txt
 pylint *.py
 docker compose build $SSH_ORIGINAL_COMMAND
 docker compose up -d $SSH_ORIGINAL_COMMAND
