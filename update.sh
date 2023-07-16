307e78dead0c439af6af67041cb34537502997d3
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-16 22:18:27 +0300

    Run pylint on the bot server before restarting it
    This should make deploy jobs fail and not deploy codes with syntax errors for example

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@ echo "Deploying $SSH_ORIGINAL_COMMAND"
 git fetch --all
 git checkout $SSH_ORIGINAL_COMMAND
 git pull
+. venv/bin/activate
+pylint *.py
 docker compose build $SSH_ORIGINAL_COMMAND
 docker compose up -d $SSH_ORIGINAL_COMMAND
