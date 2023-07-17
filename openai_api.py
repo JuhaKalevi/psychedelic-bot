be35d6e015438d064ac2b27fabce6062048f5dee
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-17 03:50:38 +0300

    Streaming fix

--- openai_api.py
+++ openai_api.py
@@ -7,8 +7,10 @@ async def openai_chat_completion(messages, model='gpt-4'):
   try:
     print('openai_chat_completion')
     async for chunk in await openai.ChatCompletion.acreate(model=model, messages=messages, stream=True):
+      response = ''
       content = chunk["choices"][0].get("delta", {}).get("content")
       if content is not None:
-        print(content, end='')
+        response += content
+      return response
   except (openai.error.APIConnectionError, openai.error.APIError, openai.error.AuthenticationError, openai.error.InvalidRequestError, openai.error.PermissionError, openai.error.RateLimitError, openai.error.ServiceUnavailableError, openai.error.Timeout) as err:
     return f"OpenAI API Error: {err}"
