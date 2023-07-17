2c7698a1f094b80968e0003e6ed5e22c56735e92
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-17 21:58:04 +0300

    Fix context_manager

--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -55,6 +55,7 @@ async def context_manager(event):
         async for chunk in generate_text.from_context(context):
           stream_chunks.append(chunk)
           reply_id = await mattermost_api.create_or_update_post(bot, {'channel_id':post['channel_id'], 'message':''.join(stream_chunks), 'file_ids':file_ids, 'root_id':reply_to}, reply_id)
+        break
 
 async def respond_to_magic_words(post, file_ids):
   word = post['message'].lower()
