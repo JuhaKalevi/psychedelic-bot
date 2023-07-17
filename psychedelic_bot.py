3152ebb2049182109aa98cf763d3b85a502ea75b
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-17 04:46:03 +0300

    Async fix

--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -46,7 +46,7 @@ async def context_manager(event):
   else:
     context = await mattermost_api.thread_context(bot, post)
     for post in context['posts'].values():
-      if await bot_name_in_message(post['message']):
+      if bot_name_in_message(post['message']):
         response = await generate_text.from_context(context)
         return await mattermost_api.create_post(bot, {'channel_id':post['channel_id'], 'message':response, 'file_ids':file_ids, 'root_id':reply_to})
 
