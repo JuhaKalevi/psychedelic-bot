37c8e6e282359374a5ee08cd7699f653c316b2e4
Juha Kallio <bunnyh@psychedelic.fi>
2023-07-24 19:30:33 +0300

    Test image generation replies directly to channel

--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -43,7 +43,7 @@ async def delegated_post_handler(lock, post):
     else:
       image_generation_comment = None
     if image_generation_comment:
-      return await mattermost_api.create_or_update_post(bot, {'channel_id':post['channel_id'], 'message':image_generation_comment, 'file_ids':file_ids, 'root_id':reply_to})
+      return await mattermost_api.create_or_update_post(bot, {'channel_id':post['channel_id'], 'message':image_generation_comment, 'file_ids':file_ids, 'root_id':''})
     if await generate_text.is_asking_for_channel_summary(post):
       context = await bot.posts.get_posts_for_channel(post['channel_id'])
     else:
