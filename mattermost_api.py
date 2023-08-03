9ad4802ec97bfde2915d62cb7e6cda42b7a46c39
Juha Kallio <bunnyh@psychedelic.fi>
2023-08-03 04:49:33 +0300

    More emoji fixes

--- mattermost_api.py
+++ mattermost_api.py
@@ -23,7 +23,10 @@ class MattermostBot(mattermostdriver.AsyncDriver):
   async def tag_post_with_emoji(self, post_id:str, emoji:str):
     try:
       user_id = await self.users.get_user_by_username(self.name)
-      reaction = await self.reactions.create_reaction(options={'user_id': user_id, 'post_id':post_id, 'emoji_name':emoji})
+      logger.debug(user_id)
+      logger.debug(post_id)
+      logger.debug(emoji)
+      reaction = await self.reactions.create_reaction(options={'user_id':user_id, 'post_id':post_id, 'emoji_name':emoji})
       return reaction
     except mattermostdriver.exceptions.ResourceNotFound as err:
       print(f'ERROR mattermost.reactions.create_reaction(): {err}')
