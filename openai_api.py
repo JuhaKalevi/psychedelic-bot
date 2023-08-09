e19dac2b3605c85873236e033257a2e32153d390
Juha Kallio <bunnyh@psychedelic.fi>
2023-08-09 13:07:52 +0300

    Improve image generation resolution description for function calls

--- openai_api.py
+++ openai_api.py
@@ -56,7 +56,9 @@ functions = [
         },
         "resolution": {
           "type":"string",
-          "description":"Resolution for requested image"
+          "description":"Resolution for requested image."
+                        " Default resolution is 1024x1024, and it has to be in this format even if the user describes it in a different way, for example 1080p translates to 1920x1080."
+                        " It's also possible to just describe shape of the image, and it should also be translated to the standard format using 1024x1024 as the starting point."
         }
       },
       "required": ["prompt","negative_prompt","count"]
