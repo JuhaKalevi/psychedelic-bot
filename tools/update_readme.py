c0ed121c918eeb7597b980d94efc71f9855fed10
Juha Kallio <bunnyh@psychedelic.fi>
2023-08-19 06:44:13 +0300

    Update README.md & tools/update_readme.py

--- tools/update_readme.py
+++ tools/update_readme.py
@@ -1,27 +1,11 @@
-import os
+import glob
 import openai
 
-openai.api_key = os.environ['OPENAI_API_KEY']
-
 summaries = []
-for path, dirs, files in os.walk('.'):
-  for file in files:
-    if file.endswith(".py"):
-      with open(os.path.join(path, file), "r", encoding="utf-8") as source:
-        text = source.read()
-        response = openai.ChatCompletion.create(
-          model="gpt-4",
-          prompt=f"Summarize the following Python code:\n```python\n{text}\n```",
-          temperature=0.3,
-          max_tokens=420
-        )
-        summaries.append(f"# {file}\n{response.choices[0].text.strip()}")
-SOURCE_SUMMARIES = '\n'.join(summaries)
-response = openai.Completion.create(
-  model="gpt-4",
-  prompt=f"Generate a README for a project with the following source files:\n{SOURCE_SUMMARIES}",
-  temperature=0.3,
-  max_tokens=1337
-)
-with open("README.md", "w", encoding="utf-8") as readme_file:
-  readme_file.write(response.choices[0].text.strip())
+for path in glob.glob('*.py'):
+  print(path)
+  with open(path, encoding='utf-8') as file:
+    code_block = f'\n```python\n{file.read()}\n```'
+    summaries.append(f"# {path}\n{openai.ChatCompletion.create(model='gpt-3.5-turbo-16k', messages=[{'role':'user','content':f'Summarize the following Python code:{code_block}'}])['choices'][0]['message']}")
+with open('README.md', 'w', encoding='utf-8') as readme_file:
+  readme_file.write(openai.ChatCompletion.create(model='gpt-4', messages=[{'role':'user','content':'Generate a README for a project with the following source files: {}'.format('\n'.join(summaries))}], temperature=0.3, max_tokens=1337)['choices'][0]['message']['content'])
