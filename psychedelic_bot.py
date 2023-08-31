b7a2fadad83fc2a4d73d438ff272fc5220b931a3
Juha Kallio <bunnyh@psychedelic.fi>
2023-08-31 04:25:52 +0300

    Remove unnecessary function return type declarations

--- psychedelic_bot.py
+++ psychedelic_bot.py
@@ -5,14 +5,14 @@ import mattermost_post_handler
 
 bot = mattermost_api.bot
 
-async def context_manager(_event:str) -> None:
+async def context_manager(_event:str):
   event = json.loads(_event)
   if event.get('event') == 'posted' and event['data']['sender_name'] != bot.name:
     post = json.loads(event['data']['post'])
     if 'from_bot' not in post['props']:
       asyncio.create_task(mattermost_post_handler.MattermostPostHandler(post).post_handler())
 
-async def main() -> None:
+async def main():
   await bot.login()
   await bot.init_websocket(context_manager)
 
