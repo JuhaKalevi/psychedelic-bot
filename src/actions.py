681f3734678b6ac4b06084e2dc740e6b40f37ac6
Juha Kallio <bunnyh@psychedelic.fi>
2023-10-05 13:25:56 +0300

    Add elapsed time to image generation comments

--- src/actions.py
+++ src/actions.py
@@ -166,6 +166,7 @@ class Mattermost():
       yield part
 
   async def generate_images(self, prompt, negative_prompt, count, resolution='1024x1024'):
+    start_time = time.time()
     bot = self.bot
     width, height = resolution.split('x')
     post = self.post
@@ -182,7 +183,7 @@ class Mattermost():
         uploaded_file_id = await bot.upload_file(post['channel_id'], {'files':(tmp_path.split('/')[2], image_file)})
         file_ids.append(uploaded_file_id)
       os.remove(tmp_path)
-    await bot.create_or_update_post({'channel_id':post['channel_id'], 'message':f"prompt: {prompt}\nnegative_prompt: {negative_prompt}\nresolution: {resolution}", 'file_ids':file_ids, 'root_id':''})
+    await bot.create_or_update_post({'channel_id':post['channel_id'], 'message':f"prompt: {prompt}\nnegative_prompt: {negative_prompt}\nresolution: {resolution}\ntime_elapsed: {start_time-time.time()}", 'file_ids':file_ids, 'root_id':''})
 
   async def get_current_weather(self, location):
     weatherapi_response = json.loads(requests.get(f"https://api.weatherapi.com/v1/current.json?key={os.environ['WEATHERAPI_KEY']}&q={location}", timeout=7).text)
