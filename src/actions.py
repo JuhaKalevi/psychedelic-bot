94cde3abd0c48ad9a363d0ca9b790b8c78424598
Juha Kallio <bunnyh@psychedelic.fi>
2023-11-13 17:07:22 +0200

    Use low detail in analyze_images function

--- src/actions.py
+++ src/actions.py
@@ -127,7 +127,7 @@ class Mattermost():
           img_byte = temp_file.read()
         remove(f'/tmp/{post_file_path}')
         base64_image = base64.b64encode(img_byte).decode("utf-8")
-        content.append({'type':'image_url','image_url':{'url':f'data:image/{file_type};base64,{base64_image}'}})
+        content.append({'type':'image_url','image_url':{'url':f'data:image/{file_type};base64,{base64_image}','detail':'low'}})
     await self.stream_reply_to_messages([{'role':'user', 'content':json.dumps(content)}], model='gpt-4-vision-preview')
 
   async def channel_summary(self, count:int):
