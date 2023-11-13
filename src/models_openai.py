387327ed3ef3d54610e8b05fbc87e50a6c7982d9
Juha Kallio <bunnyh@psychedelic.fi>
2023-11-13 17:42:03 +0200

    More fixes for OpenAI Python SDK v1.2

--- src/models_openai.py
+++ src/models_openai.py
@@ -143,9 +143,9 @@ client = AsyncOpenAI()
 
 async def chat_completion_functions(messages:list, available_functions:dict):
   try:
-    response = await client.chat.completions.create(messages=messages, functions=function_descriptions, model='gpt-4-1106-preview')
-    response_message = response['choices'][0]['message']
-    if response_message.get("function_call"):
+    completion = await client.chat.completions.create(messages=messages, functions=function_descriptions, model='gpt-4-1106-preview')
+    response_message = completion.choices[0].message
+    if completion.get("function_call"):
       function = response_message["function_call"]["name"]
       arguments = loads(response_message["function_call"]["arguments"])
       await available_functions[function](**arguments)
@@ -158,9 +158,8 @@ async def chat_completion_streamed(messages:list, functions=None, model='gpt-4-1
     kwargs = {"messages":messages, "model":model, "stream":True}
     if functions:
       kwargs["functions"] = functions
-    async for chunk in await client.chat.completions.create(**kwargs):
-      content = chunk["choices"][0].get("delta", {}).get("content")
-      if content:
-        yield content
+    async for part in await client.chat.completions.create(**kwargs):
+      content = part.choices[0].delta.content or ""
+      yield content
   except APIError as err:
     print(f"OpenAI API Error: {err}")
