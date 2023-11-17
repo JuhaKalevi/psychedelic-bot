d37d2e75673d78273f68c29cb87749580163bd81
Juha Kallio <bunnyh@psychedelic.fi>
2023-11-17 13:32:35 +0200

    Don't use json.dumps() for gpt-4-vision-preview content

--- src/actions.py
+++ src/actions.py
@@ -128,7 +128,7 @@ class Mattermost():
         remove(f'/tmp/{post_file_path}')
         base64_image = base64.b64encode(img_byte).decode("utf-8")
         content.append({'type':'image_url','image_url':{'url':f'data:image/{file_type};base64,{base64_image}','detail':'low'}})
-    await self.stream_reply_to_messages([{'role':'user', 'content':json.dumps(content)}], model='gpt-4-vision-preview')
+    await self.stream_reply_to_messages([{'role':'user', 'content':content}], model='gpt-4-vision-preview')
 
   async def channel_summary(self, count:int):
     self.context = await self.bot.posts.get_posts_for_channel(self.post['channel_id'], params={'per_page':count})
