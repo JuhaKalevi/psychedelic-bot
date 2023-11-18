cf6cd292c86f91510d79836c9b4aa2d04f782ae4
Juha Kallio <bunnyh@psychedelic.fi>
2023-11-18 19:43:09 +0200

    Add logic missing from last commit

--- src/models_openai.py
+++ src/models_openai.py
@@ -149,7 +149,7 @@ async def chat_completion_functions(messages:list, available_functions:dict):
       funcs.append({'name':f['name'],'description':f['description'],'parameters':f['parameters']})
       funcs_stub.append({"name":f['name'],"description":"","parameters":{"type":"object","properties":{}}})
     print(len(dumps(function_descriptions)), len(dumps(funcs_stub)))
-    completion = await client.chat.completions.create(messages=messages, functions=function_descriptions, model='gpt-4-1106-preview')
+    completion = await client.chat.completions.create(messages=messages, functions=funcs, model='gpt-4-1106-preview')
     response_message = completion.choices[0].message
     if dict(response_message).get("function_call"):
       function = response_message.function_call.name
