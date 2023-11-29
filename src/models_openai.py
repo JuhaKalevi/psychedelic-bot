447627fe423677afa637be7a116c1a724b7ce509
Juha Kallio <bunnyh@psychedelic.fi>
2023-11-29 14:12:45 +0200

    More fixes to choose_function JSON schema

--- src/models_openai.py
+++ src/models_openai.py
@@ -84,7 +84,7 @@ async def chat_completion_functions(msgs:list, f_avail:dict):
   f_choose = [
     {
       'name': 'choose_function',
-      'description': "This function is used to select which of the actual functions should be called. If the user mentions a function by name but the message doesn't really seem to be a request for that function, text_response_default or instant_self_code_analysis could be suitable.",
+      'description': "This function is used to select which of the actual functions should be called. If the user mentions a function by name but the message doesn't really seem to be a request for that function, don't call it then!",
       'parameters': {
         'type': 'object',
         'properties': {
