282000941470c7062dd24d5d9977b594a2ef250f
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-06 15:19:56 +0200

    Compactify some generic code in actions.py

--- src/actions.py
+++ src/actions.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from asyncio import create_task
 import base64
-import json
+from json import dumps, loads
 from os import environ, listdir
 from time import ctime
 import requests
@@ -29,22 +29,23 @@ class Actions(ABC):
   async def __post_handler__(self):
     pass
 
+  @abstractmethod
+  async def messages_from_context(self, count:int=None, max_tokens:int=None) -> list[dict[str, str]]:
+    pass
+
+  @abstractmethod
+  async def stream_reply(self, msgs:list, model='', max_tokens:int=None):
+    pass
+
   async def channel_summary(self, count:int):
     msgs = await self.messages_from_context(count)
     await self.generic('channel_summary', {'count':len(msgs)}, msgs)
 
-  async def generic(self, function:str, arguments:dict, response:dict):
-    '''Generic function call that can be used with some simpler functions'''
-    messages = [
-      {"role": "user", "content": self.content},
-      {"role": "assistant", "content": None, "function_call": {"name": function, "arguments": json.dumps(arguments)}},
-      {"role": "function", "name": function, "content": json.dumps(response)}
-    ]
-    await self.stream_reply(messages)
+  async def generic(self, func:str, args:dict, resp:dict):
+    await self.stream_reply([{'role':'user','content':self.content}, {'role':'assistant','content':None,'function_call':{'name':func,'arguments':dumps(args)}}, {'role':'function','name':func,'content':dumps(resp)}])
 
   async def get_current_weather(self, location:str):
-    weatherapi_response = json.loads(requests.get(f"https://api.weatherapi.com/v1/current.json?key={environ['WEATHERAPI_KEY']}&q={location}", timeout=7).text)
-    await self.generic('get_current_weather', {'location':location}, weatherapi_response)
+    await self.generic('get_current_weather', {'location':location}, loads(requests.get(f"https://api.weatherapi.com/v1/current.json?key={environ['WEATHERAPI_KEY']}&q={location}",timeout=7).text))
 
   async def instant_self_code_analysis(self):
     '''Inserts all these source files to the context so they can be analyzed. This is a hacky way to do it, but it works.'''
@@ -55,14 +56,6 @@ class Actions(ABC):
       files.append(f'\n--- BEGIN {file_path} ---\n```\n{content}\n```\n--- END {file_path} ---\n')
     self.instructions[0]['content'] = f"\nThis is your code. Abstain from posting parts of your code unless discussing changes to them. Use PEP-8 but 2 spaces for indentation, try to keep it minimalistic; don't use comments at all! Abstain from praising or thanking the user, be serious.{''.join(files)}{self.instructions[0]['content']}"
     await self.stream_reply(await self.messages_from_context())
-
-  @abstractmethod
-  async def messages_from_context(self, count:int=None, max_tokens:int=None) -> list[dict[str, str]]:
-    pass
-
-  @abstractmethod
-  async def stream_reply(self, msgs:list, model='gpt-3.5-turbo-16k', max_tokens:int=None):
-    pass
 
   async def text_response_default(self):
     '''Default function that can be called when a normal text response suffices'''
