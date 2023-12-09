4f3ad43cc87221a189b9c4121799b39932c2e5d3
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-09 07:23:22 +0200

    Use gpt-4-1106-preview in second stage decision making

--- src/openai_models.py
+++ src/openai_models.py
@@ -45,7 +45,7 @@ async def chat_completion_functions(msgs:list, f_avail:dict):
         f_avail = {f: f_avail[f] for f in f_avail if f in [fdict['name'] for fdict in f_img+f_default]}
       elif f_required_context['modality'] == 'text':
         f_avail = {f: f_avail[f] for f in f_avail if f in [fdict['name'] for fdict in f_default+f_txt]}
-      f_choice = await chat_completion_choices(msgs[-int(f_required_context['posts']):], f_avail, f_choose, ['function_name'], 'gpt-3.5-turbo-1106')
+      f_choice = await chat_completion_choices(msgs[-int(f_required_context['posts']):], f_avail, f_choose, ['function_name'], 'gpt-4-1106-preview')
       f_choice = f_choice['function_name']
       if f_required_context['modality'] == 'image':
         f_description = next(([f] for f in f_img+f_default if f['name'] == f_choice), [])
