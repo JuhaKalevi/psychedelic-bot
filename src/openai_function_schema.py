86577e3803eff5d260b21786d35551c5e34f5cc7
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-09 05:50:07 +0200

    Use "action suggested" instead of "action requested" wording

--- src/openai_function_schema.py
+++ src/openai_function_schema.py
@@ -19,7 +19,7 @@ f_estimate_required_context = [
         },
         'posts': {
           'type': 'integer',
-          'description': "See more posts (including your own previous replies) to decide next action. Use 0 if asked about functions or no explicit action requested! For affirmations or negations use at least 1, and consider otherwise vague messages as 2 or more.",
+          'description': "See more posts (including your own previous replies) to decide next action. Use 0 if asked about functions or no explicit action suggested! For affirmations or negations use at least 1, and consider otherwise vague messages as 2 or more.",
           'enum': [0,1,2,3,4]
         }
       },
