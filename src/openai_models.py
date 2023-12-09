f928db3984b078ea7420dc4084cc88a530219214
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-09 07:32:13 +0200

    Back to gpt-4-1106-preview

--- src/openai_models.py
+++ src/openai_models.py
@@ -37,7 +37,7 @@ async def chat_completion_functions(msgs:list, f_avail:dict):
     }
   ]
   try:
-    f_required_context = await chat_completion_choices(msgs[-1:], {}, f_estimate_required_context, ['modality','posts'], 'gpt-3.5-turbo-1106')
+    f_required_context = await chat_completion_choices(msgs[-1:], {}, f_estimate_required_context, ['modality','posts'], 'gpt-4-1106-preview')
     if f_required_context['modality'] == 'image' and f_required_context['posts'] == 0:
       f_required_context['posts'] = 1
     if int(f_required_context['posts']):
