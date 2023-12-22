59e7ec396789c77d0ade4de8275f440c5dc9170e
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-22 22:29:38 +0200

    Debug print len(current_context)/len(full_context

--- src/openai_models.py
+++ src/openai_models.py
@@ -3,20 +3,21 @@ from openai import AsyncOpenAI
 from helpers import count_tokens
 from openai_function_schema import actions, empty_params, semantic_analysis, intention_analysis
 
-async def react(msgs:list, available_functions:dict):
+async def react(full_context:list, available_functions:dict):
   client = AsyncOpenAI()
   try:
     semantic_analysis_attempts = 0
     semantic_analysis_confidence = 0
     while semantic_analysis_attempts < 3 and semantic_analysis_confidence < 0.85:
-      context = msgs[-1-semantic_analysis_attempts:]
       semantic_analysis_attempts += 1
-      semantics = await think(context, semantic_analysis(len(context)/len(msgs)), 'gpt-3.5-turbo-16k')
+      current_context = full_context[-semantic_analysis_attempts:]
+      print(len(current_context)/len(full_context))
+      semantics = await think(current_context, semantic_analysis(len(current_context)/len(full_context)), 'gpt-3.5-turbo-16k')
     intention = await think([{'role':'user','content':semantics['analysis']}], intention_analysis(list(available_functions)), 'gpt-4-1106-preview')
     action = intention['next_action']
     action_description = next(([f] for f in actions if f['name'] == action), [])
     if action_description[0]['parameters'] != empty_params:
-      action_arguments_completion = await client.chat.completions.create(messages=msgs, functions=action_description, function_call={'name':action}, model='gpt-4-1106-preview')
+      action_arguments_completion = await client.chat.completions.create(messages=full_context, functions=action_description, function_call={'name':action}, model='gpt-4-1106-preview')
       arguments = loads(action_arguments_completion.choices[0].message.function_call.arguments)
       await available_functions[action](**arguments)
     else:
