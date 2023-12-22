0ba55007174af5658bb6e918146ccc2c7faf95e6
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-22 22:58:42 +0200

    Stop semantic analysis after confidence thershold

--- src/openai_models.py
+++ src/openai_models.py
@@ -8,11 +8,11 @@ async def react(full_context:list, available_functions:dict):
   try:
     semantic_analysis_attempts = 0
     semantic_analysis_confidence = 0
-    while semantic_analysis_attempts < 3 and semantic_analysis_confidence < 0.85:
+    while semantic_analysis_attempts < 3:
       semantic_analysis_attempts += 1
       current_context = full_context[-semantic_analysis_attempts:]
       semantics = await think(current_context, semantic_analysis(len(current_context)/len(full_context)), 'gpt-3.5-turbo-1106')
-      if current_context == full_context:
+      if current_context == full_context or semantic_analysis_confidence > 0.85:
         break
     intention = await think([{'role':'user','content':semantics['analysis']}], intention_analysis(list(available_functions)), 'gpt-4-1106-preview')
     action = intention['next_action']
