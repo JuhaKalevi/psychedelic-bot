72adf1488a64890a2490fac1e845b498df2fc2d6
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-22 22:40:32 +0200

    Debug print recalled_context_fraction

--- src/openai_function_schema.py
+++ src/openai_function_schema.py
@@ -6,6 +6,7 @@ IMGGEN_REMIND = "Don't use any kind of formatting to separate these keywords, ex
 empty_params = {'type':'object','properties':{}}
 
 def semantic_analysis(recalled_context_fraction):
+  print(recalled_context_fraction)
   return {
     'name': 'semantic_analysis',
     'description': f"Provide ONLY a concise semantic analysis of the message, DO NOT actually answer like you normally would! Note that the message can be in any language. Think and think again, could the message be unclear due to context being partially missing? You currently see {round(recalled_context_fraction*100)}% of the context.",
--- src/openai_models.py
+++ src/openai_models.py
@@ -11,7 +11,6 @@ async def react(full_context:list, available_functions:dict):
     while semantic_analysis_attempts < 3 and semantic_analysis_confidence < 0.85:
       semantic_analysis_attempts += 1
       current_context = full_context[-semantic_analysis_attempts:]
-      print(len(current_context)/len(full_context))
       semantics = await think(current_context, semantic_analysis(len(current_context)/len(full_context)), 'gpt-3.5-turbo-1106')
     intention = await think([{'role':'user','content':semantics['analysis']}], intention_analysis(list(available_functions)), 'gpt-4-1106-preview')
     action = intention['next_action']
