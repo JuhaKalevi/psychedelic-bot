d9603e21a5f9831620b02a2853668ccf75729af9
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-23 04:19:14 +0200

    Debug print event_classifications in react

--- src/openai_models.py
+++ src/openai_models.py
@@ -8,9 +8,9 @@ from openai_function_schema import actions, EMPTY_PARAMS
 async def react(full_context:list, available_functions:dict):
   client = AsyncOpenAI()
   action = 'chat'
-  print(f"react: available_functions: {available_functions}")
   event_classifications_object = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")(full_context[-1]['content'], ['self_analysis_request','image_generation_request'], multi_label=True)
   event_classifications = dict(zip(event_classifications_object['labels'], event_classifications_object['scores']))
+  print(f"event_classifications:{event_classifications}")
   if event_classifications_object['labels'][0] == 'image_generation_request':
     if event_classifications['image_generation_request'] > 0.8 and event_classifications['self_analysis_request'] < 0.2:
       action = 'generate_images'
