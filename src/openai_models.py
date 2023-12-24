ea11d9ad529dc40a6eca0d936d6be294e692972c
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-25 00:11:14 +0200

    Change EVENT_CATEGORIES

--- src/openai_models.py
+++ src/openai_models.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 from helpers import count_tokens
 from openai_function_schema import ANALYZE_SELF, GENERATE_IMAGES, translate_to_english, actions, EMPTY_PARAMS
 
-EVENT_CATEGORIES = ['Command', 'Question', 'Second-Person Reference']
+EVENT_CATEGORIES = ['Command', 'Question', 'Second Person']
 
 async def chat(msgs, model='gpt-4-1106-preview', max_tokens=None):
   client = AsyncOpenAI()
