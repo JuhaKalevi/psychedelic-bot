7c3a54b5c5ed1555e11cfa080ce3ca64a1c4e7ad
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-28 09:17:21 +0200

    Compactify code

--- src/openai_models.py
+++ src/openai_models.py
@@ -3,7 +3,10 @@ from openai import AsyncOpenAI
 from transformers import pipeline
 from openai_function_schema import translate_to_english, ACTIONS, EMPTY_PARAMS
 
-EVENT_CATEGORIES = ['Instructions given to the chatbot to (often implicitly) generate described images.', 'Messages that address the chatbot directly or discuss its functions, capabilities, or status.']
+EVENT_ANALYZE_SELF = 'Messages that address the chatbot directly or discuss its functions, capabilities, or status.'
+EVENT_GENERATE_IMAGES = 'Instructions given to the chatbot to (often implicitly) generate described images.'
+
+EVENT_CATEGORIES = [EVENT_ANALYZE_SELF, EVENT_GENERATE_IMAGES]
 
 async def background_function(kwargs):
   completion = ''
@@ -11,7 +14,6 @@ async def background_function(kwargs):
     if delta.function_call:
       completion += delta.function_call.arguments
     else:
-      print(completion)
       return {d:loads(completion)[d] for d in list(kwargs['functions'][0]['parameters']['properties'])}
 
 async def chat_completion(kwargs):
@@ -19,28 +21,21 @@ async def chat_completion(kwargs):
   async for part in await client.chat.completions.create(**kwargs, stream=True):
     yield part.choices[0].delta
 
-async def classify(event_translation, labels=None):
-  print(event_translation)
-  if not labels:
-    labels = EVENT_CATEGORIES
-  zero_shot_classifications_object = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")(event_translation, labels, multi_label=True)
-  event_classifications = dict(zip(zero_shot_classifications_object['labels'], zero_shot_classifications_object['scores']))
-  print(event_classifications)
-  action = 'Chat'
-  print(action)
-  return action
-
 async def react(full_context:list, available_functions:dict):
   action = 'Chat'
   if full_context[0] in full_context[-3:]:
     context = full_context[-3:]
   else:
     context = full_context[:1] + full_context[-3:]
-  print(context[1:])
   context_interactions_in_english = await background_function({'messages':context[1:], 'functions':[translate_to_english], 'function_call':{'name':translate_to_english['name']}, 'model':'gpt-4-1106-preview'})
-  print(context_interactions_in_english)
   event_translation = f"System message:\n{full_context[0]['content']}\n\nInteractions:\n{context_interactions_in_english['translation']}"
-  action = await classify(event_translation)
+  zero_shot_classifications_object = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")(event_translation, EVENT_CATEGORIES, multi_label=True)
+  event_classifications = dict(zip(zero_shot_classifications_object['labels'], zero_shot_classifications_object['scores']))
+  print(event_classifications)
+  if event_classifications[EVENT_ANALYZE_SELF] > 0.8 and event_classifications[EVENT_GENERATE_IMAGES] < 0.2:
+    action = EVENT_ANALYZE_SELF
+  elif event_classifications[EVENT_GENERATE_IMAGES] > 0.8 and event_classifications[EVENT_ANALYZE_SELF] < 0.2:
+    action = EVENT_GENERATE_IMAGES
   action_description = next(([f] for f in ACTIONS if f['name'] == action), [])
   if action != 'Chat' and action_description[0]['parameters'] != EMPTY_PARAMS:
     arguments_completion_kwargs = await background_function({'messages':full_context, 'functions':[action_description], 'function_call':{'name':action}, 'model':'gpt-4-1106-preview', 'temperature':0})
