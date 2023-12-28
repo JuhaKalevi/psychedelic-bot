b3ec36b1a30e2de922f27e081c5e781688189f62
Juha Kallio <bunnyh@psychedelic.fi>
2023-12-28 19:22:17 +0200

    Update labels

--- src/openai_models.py
+++ src/openai_models.py
@@ -7,7 +7,7 @@ event_labels = {
  'code_analysis':'Analysis of code, functions or capabilities.',
 }
 action_labels = {
-  'analyze_self':'Analysis of your features.',
+  'analyze_self':'Message refers to you.',
   'generate_images':'Instructions that clearly describe an image the user wants to generate.',
 }
 zero_shot_classification_pipeline = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
