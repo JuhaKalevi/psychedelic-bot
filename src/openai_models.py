359d1a17ad68aee8c374a43b7a726ade85b24f25
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 18:58:06 +0200

    Inclue previous message in threads if available for semantic analysis

--- src/openai_models.py
+++ src/openai_models.py
@@ -28,7 +28,10 @@ async def consider(kwargs):
         return completion
 
 async def react(context:list, available_functions:dict):
-  semantics_prompt = f'Convey in english the semantic meaning of the following message:\n\n{context[-1]}'
+  if len(context) < 3:
+    semantics_prompt = f'Convey in english the semantic meaning of the following message:\n\n{context[-1]}'
+  else:
+    semantics_prompt = f'Convey in english the semantic meaning of the following conversation flow:\n\nPrevious message:{context[-2]}\n\nCurrent message:{context[-1]}'
   semantics = await consider({'prompt':semantics_prompt, 'model':'gpt-3.5-turbo-instruct', 'temperature':0, 'max_tokens':4096-count_tokens(semantics_prompt)})
   self_analysis_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
