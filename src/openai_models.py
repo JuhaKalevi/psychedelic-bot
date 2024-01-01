7a4348edd1de015a000e76011ea240db3ea3399c
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 23:15:04 +0200

    Revert to translation_reflection

--- src/openai_models.py
+++ src/openai_models.py
@@ -1,6 +1,5 @@
 from json import loads
 from openai import AsyncOpenAI
-from helpers import count_tokens
 from openai_function_schema import generate_images_schema
 
 async def chat_completion(kwargs):
@@ -25,23 +24,23 @@ async def consider(kwargs):
 
 async def react(context:list, available_functions:dict):
   if len(context) < 3:
-    print('SINGLE MESSAGE')
-    translation = f'Reply with the following message translated to english if necessary. Just repeat the message if translation is not necessary. Message begins: {context[-1]["content"]}'
+    message = {context[-1]["content"]}
   else:
-    translation = f'Reply with the following two messages translated to english if necessary. Just repeat the messages if translation is not necessary. Messages begin: {context[-2]["content"]} {context[-1]["content"]}'
-  semantics = await consider({'prompt':translation, 'model':'gpt-3.5-turbo-1106', 'max_tokens':4096-count_tokens(translation)})
+    message = f'{context[-2]["content"]} {context[-1]["content"]}'
+    translation_reflection = {'role':'system','content':f'Your job is respond with exactly the following message in English: {message}'}
+  translation = await consider({'messages':translation_reflection, 'model':'gpt-3.5-turbo-1106'})
   self_analysis_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities! Reply 1 if the message is requesting analysis of your capabilities, and 0 if it is not!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting analysis of MY capabilities. I will NEVER reply anything else!'},
-    {'role':'user','content':semantics},
+    {'role':'user','content':translation},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   image_generation_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for the generation of images using a local API.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting image generation!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting images. I will NEVER reply anything else!'},
-    {'role':'user','content':semantics},
+    {'role':'user','content':translation},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   if await consider({'messages':self_analysis_reflection, 'model':'gpt-3.5-turbo-1106', 'temperature':0, 'max_tokens':1}) == '1':
