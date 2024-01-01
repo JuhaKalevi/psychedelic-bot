385121a0d2ade2c87b66e9d4c8499fd51ee19419
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 12:07:02 +0200

    Ditch zero shot classification, revert to OpenAI

--- src/openai_models.py
+++ src/openai_models.py
@@ -1,9 +1,6 @@
 from json import loads
 from openai import AsyncOpenAI
-from transformers import pipeline
 from openai_function_schema import ACTIONS
-
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
 async def background_function(kwargs):
   completion = ''
@@ -27,22 +24,14 @@ async def chat_completion(kwargs):
   async for part in await client.chat.completions.create(**kwargs, stream=True):
     yield part.choices[0].delta
 
-def classify(message, labels):
-  if len(labels) > 1:
-    classification = classifier(message, labels)
-    return dict(zip(classification['labels'], classification['scores']))
-  return classifier(message, labels[0])['scores'][0]
-
 async def react(context:list, available_functions:dict):
   action = 'Chat'
   translation = await background_function({'messages':[{'role':'system','content':'Just translate this message to english instead of replying normally'}, context[-1]], 'model':'gpt-3.5-turbo-1106', 'temperature':0})
   meaning = await background_function({'messages':[{'role':'user','content':f'ONLY describe the meaning of the rest of this message INSTEAD OF REPLYING NORMALLY:\n{translation}'}], 'model':'gpt-3.5-turbo-1106', 'temperature':0})
-  if classify(meaning, ['Explain yourself']) > 0.8:
-    print('DO analyze_self')
+  if await background_function({'messages':[{'role':'user','content':f'ONLY answer should you load your chatbot source code into the context considering this:\n{meaning}'}], 'model':'gpt-3.5-turbo-1106', 'temperature':0, 'max_tokens':1}) > 0.8:
     action = 'analyze_self'
-  elif classify(meaning, ['Image requested']) > 0.8:
-      print('DO generate_images')
-      action = 'generate_images'
+  elif await background_function({'messages':[{'role':'user','content':f'ONLY answer should you use your image generation capability considering this:\n{meaning}'}], 'model':'gpt-3.5-turbo-1106', 'temperature':0, 'max_tokens':1}) > 0.8:
+    action = 'generate_images'
   action_arguments = next(([f] for f in ACTIONS if f['name'] == action), [])
   if action != 'Chat' and action_arguments:
     await available_functions[action](**await background_function({'messages':context, 'functions':action_arguments, 'function_call':{'name':action}, 'model':'gpt-4-1106-preview'}))
