9e29898b12946465a1c6f456d1fb8f4a4825e701
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 23:20:14 +0200

    Return transaction_reflection to old form

--- src/openai_models.py
+++ src/openai_models.py
@@ -27,7 +27,13 @@ async def react(context:list, available_functions:dict):
     message = {context[-1]["content"]}
   else:
     message = f'{context[-2]["content"]} {context[-1]["content"]}'
-  translation_reflection = [{'role':'system','content':f'Your job is respond with exactly the following message in English: {message}'}]
+  translation_reflection = [
+    {'role':'system','content':f'You are a TRANSLATOR that is ONLY allowed to respond with a translation of the message to english.'},
+    {'role':'user','content':'From now on ONLY translate messages to english!'},
+    {'role':'assistant','content':'Understood! I will ONLY answer with translations to your messages. I will NEVER reply anything else!'},
+    {'role':'user','content':message},
+    {'role':'user','content':f'Your job is translate this message to english if necessary and then reply with the translation or the original message if it was in english.'}
+    ]
   translation = await consider({'messages':translation_reflection, 'model':'gpt-3.5-turbo-1106'})
   self_analysis_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
