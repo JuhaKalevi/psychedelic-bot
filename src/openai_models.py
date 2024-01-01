1ffad80e3d0c20259dffcf3b0b684eba39ca6fe9
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 14:14:32 +0200

    Make assistant swear to respond only 1 or 0

--- src/openai_models.py
+++ src/openai_models.py
@@ -32,15 +32,15 @@ async def react(context:list, available_functions:dict):
   ]
   translation = await consider({'messages':translation_reflection, 'model':'gpt-3.5-turbo-1106', 'temperature':0})
   self_analysis_reflection = [
-    {'role':'system','content':'You are a validator for true/false questions.'},
-    {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities.'},
-    {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting analysis of MY capabilities.'},
+    {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
+    {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities!'},
+    {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting analysis of MY capabilities. I will NEVER reply anything else!'},
     {'role':'user','content':translation}
   ]
   image_generation_reflection = [
-    {'role':'system','content':'You are a validator for true/false questions.'},
-    {'role':'user','content':'From now on ONLY classify whether messages are requesting image generation.'},
-    {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting images.'},
+    {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for the generation of images using a local API.'},
+    {'role':'user','content':'From now on ONLY classify whether messages are requesting image generation!'},
+    {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting images. I will NEVER reply anything else!'},
     {'role':'user','content':translation}
   ]
   action = 'Chat'
