73fb3b1e027b7c62fbe2f4def55bc61ec21b6a8a
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 17:41:32 +0200

    Fix use of semantics object

--- src/openai_models.py
+++ src/openai_models.py
@@ -27,19 +27,19 @@ async def consider(kwargs):
         return completion
 
 async def react(context:list, available_functions:dict):
-  user_content_semantics = await consider({'prompt':f'Act as NLP PROCESSOR and convey in english the semantic meaning of the following message:\n\n{context[-1]}', 'model':'gpt-3.5-turbo-instruct', 'temperature':0})
+  semantics = await consider({'prompt':f'Act as NLP PROCESSOR and convey in english the semantic meaning of the following message:\n\n{context[-1]}', 'model':'gpt-3.5-turbo-instruct', 'temperature':0})
   self_analysis_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities! Reply 1 if the message is requesting analysis of your capabilities, and 0 if it is not!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting analysis of MY capabilities. I will NEVER reply anything else!'},
-    user_content_semantics,
+    {'role':'user','content':semantics},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   image_generation_reflection = [
     {'role':'system','content':'You are a CLASSIFIER that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for the generation of images using a local API.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting image generation!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting images. I will NEVER reply anything else!'},
-    user_content_semantics,
+    {'role':'user','content':semantics},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   if await consider({'messages':self_analysis_reflection, 'model':'gpt-3.5-turbo-1106', 'temperature':0, 'max_tokens':1}) == '1':
