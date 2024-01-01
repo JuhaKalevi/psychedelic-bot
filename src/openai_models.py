f6c87fa41c6fc32294c7195d36edbb4857dff62c
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-01 11:27:18 +0200

    Use another step with gpt-3.5-turbo-1106
    First the message is translated, then its implication is determined in a subsequent call.

--- src/openai_models.py
+++ src/openai_models.py
@@ -35,17 +35,18 @@ def classify(message, labels):
 
 async def react(context:list, available_functions:dict):
   action = 'Chat'
-  translation = await background_function({'messages':[{'role':'system','content':'Just translate this message to english instead of replying normally'},context[-1]], 'model':'gpt-3.5-turbo-1106'})
-  if classify(translation, ['Analysis of code, functions or capabilities.']) > 0.6:
+  translation = await background_function({'messages':[{'role':'system','content':'Just translate this message to english instead of replying normally'}, context[-1]], 'model':'gpt-3.5-turbo-1106'})
+  implication = await background_function({'messages':[{'role':'system','content':'Just describe the implication of this message instead of replying normally'}, {'role':'user','content':translation}], 'model':'gpt-3.5-turbo-1106'})
+  if classify(implication, ['Analysis of code, functions or capabilities.']) > 0.6:
     print('CONSIDER analyze_self')
-    if classify(translation, ['Message refers to you.']) > 0.4:
+    if classify(implication, ['Message refers to you.']) > 0.4:
       print('DO analyze_self')
       action = 'analyze_self'
   else:
-    if classify(translation, ['Instructions that describe an image the user wants to generate.']) > 0.8:
+    if classify(implication, ['Instructions that describe an image the user wants to generate.']) > 0.8:
       print('DO generate_images')
       action = 'generate_images'
-    elif classify(translation, ['Confirmation of image generation request.']) > 0.8:
+    elif classify(implication, ['Confirmation of image generation request.']) > 0.8:
       print('DO generate_images')
       action = 'generate_images'
   action_arguments = next(([f] for f in ACTIONS if f['name'] == action), [])
