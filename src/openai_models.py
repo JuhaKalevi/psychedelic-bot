129d0ecd0cfb24a7372d3f5874ee3e8fd5b1ac82
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-06 20:13:44 +0200

    Debug print messages in react

--- src/openai_models.py
+++ src/openai_models.py
@@ -27,6 +27,7 @@ async def react(context:list, available_functions:dict, my_name:str):
     message = context[-1]["content"]
   else:
     message = f'{context[-2]["content"]} {context[-1]["content"]}'
+  print(message)
   self_analysis_reflection = [
     {'role':'system','content':f'You are a CLASSIFIER called {my_name} that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities! Reply 1 if the message is requesting analysis of your capabilities, and 0 if it is not!'},
