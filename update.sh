644d8d4cbbc2183dbc847767cc7d830248ec7c5f
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-06 19:36:52 +0200

    Fix requirements.txt handling
    With `--use-deprecated legacy-solver` the problem of always having to re-clone a VCS requirement is averted. There are quite valid reasons why this is deprecated.

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
 . venv/bin/activate
+pip install -r requirements.txt --use-deprecated legacy-resolver
 pylint --jobs=$(nproc) src/*.py
 systemctl --user restart psychedelic-bot@$SSH_ORIGINAL_COMMAND
