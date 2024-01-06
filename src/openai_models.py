9b16975aa9f0a56533238802ba31cfbf0531b384
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-06 20:27:06 +0200

    Use EasyNMT for translation

--- src/openai_models.py
+++ src/openai_models.py
@@ -1,6 +1,9 @@
 from json import loads
+from easynmt import EasyNMT
 from openai import AsyncOpenAI
 from openai_function_schema import generate_images_schema
+
+translator = EasyNMT('opus-mt')
 
 async def chat_completion(kwargs):
   async for part in await AsyncOpenAI().chat.completions.create(**kwargs, stream=True):
@@ -27,19 +30,20 @@ async def react(context:list, available_functions:dict, my_name:str):
     message = context[-1]["content"]
   else:
     message = f'{context[-2]["content"]} {context[-1]["content"]}'
-  print(message)
+  translation = translator.translate(message, target_lang='en')
+  print(translation)
   self_analysis_reflection = [
     {'role':'system','content':f'You are a CLASSIFIER called {my_name} that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for INCLUDING YOUR CHATBOT SOURCE CODE into the context before answering.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting analysis of YOUR chatbot capabilities! Reply 1 if the message is requesting analysis of your capabilities, and 0 if it is not!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting analysis of MY capabilities. I will NEVER reply anything else!'},
-    {'role':'user','content':message},
+    {'role':'user','content':translation},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   image_generation_reflection = [
     {'role':'system','content':f'You are a CLASSIFIER called {my_name} that is ONLY allowed to respond with 1 or 0 to DETERMINE if a message calls for the generation of images using a local API.'},
     {'role':'user','content':'From now on ONLY classify whether messages are requesting image generation!'},
     {'role':'assistant','content':'Understood! I will ONLY answer 1 or 0 to your messages, signifying if they are requesting images. I will NEVER reply anything else!'},
-    {'role':'user','content':message},
+    {'role':'user','content':translation},
     {'role':'user','content':'PLEASE REMEMBER TO ONLY REPLY 1 or 0'}
   ]
   if await consider({'messages':self_analysis_reflection, 'model':'gpt-3.5-turbo-1106', 'temperature':0, 'max_tokens':1}) == '1':
