1f9f03c4644bd0fa5dd41f5bb65ba44ca97dad19
Juha Kallio <bunnyh@psychedelic.fi>
2024-01-17 18:30:20 +0200

    Print base64 image strings before and after crop

--- src/helpers.py
+++ src/helpers.py
@@ -19,11 +19,11 @@ def count_tokens(msg):
   return len(get_encoding('cl100k_base').encode(dumps(msg)))
 
 def crop_borders(base64_image, threshold):
-  print(len(base64_image))
+  print(base64_image)
   img = cv2.imdecode(numpy.frombuffer(base64.b64decode(base64_image), dtype=numpy.uint8), cv2.IMREAD_UNCHANGED)
   _, binarized = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), threshold, 255, cv2.THRESH_BINARY_INV)
   contours, _ = cv2.findContours(binarized, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
   _, encoded_img = cv2.imencode('.png', img[y:y+h, x:x+w])
-  print(len(base64.b64encode(encoded_img).decode('utf-8')))
+  print(base64.b64encode(encoded_img).decode('utf-8'))
   return base64.b64encode(encoded_img).decode('utf-8')
